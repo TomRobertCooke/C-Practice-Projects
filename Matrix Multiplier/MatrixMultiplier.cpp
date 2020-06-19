2c03d0a930e714b3da5b22925785ce83e43fb72d
TomRobertCooke <46798275+TomRobertCooke@users.noreply.github.com>
2020-06-19 09:18:24 -0400

    Added Previous Code

--- Matrix Multiplier/MatrixMultiplier.cpp
+++ Matrix Multiplier/MatrixMultiplier.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
-#include <string>
+#include <fstream>
+#include <sstream>
 
+using namespace std;
+
+int main() {
+	bool open = false;
+	ifstream fileIn;
+	while (!open) {
+		cout << "Filename: ";
+		string filename = "";
+		getline(cin, filename);
+		fileIn.open(filename);
+		if (fileIn.is_open()) {
+			open = true;
+		}
+		else {
+			cout << "File not found, try again" << endl;
+		}
+	}
+	string line;
+	getline(fileIn, line);
+
+	return 0;
+}
